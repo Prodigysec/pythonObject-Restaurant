bada14ee58092ea7c66d72bcae64b6fd00fb3831
Ian Raburu <williamraburu@gmail.com>
2023-08-31 11:58:19 +0300

    Create empty list and append review instances

--- Review.py
+++ Review.py
@@ -1,9 +1,12 @@
 
 class Review:
+    instances = []
+
     def __init__(self, customer, restaurant, rating):
         self.customer = customer
         self.restaurant = restaurant
         self.rating = rating
+        Review.instances.append(self)
 
     def rating(self):
         return self.rating  
