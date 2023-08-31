db574f711b84ced979216b59877cea657b63a090
Ian Raburu <williamraburu@gmail.com>
2023-08-31 11:21:54 +0300

    Add given_name, family_name & full_name methods to Customer class

--- Customer.py
+++ Customer.py
@@ -4,3 +4,11 @@ class  Customer:
         self.given_name = given_name
         self.family_name = family_name
         
+    def given_name(self):
+        return self.given_name
+    
+    def family_name(self):
+        return self.family_name
+    
+    def full_name(self):
+        return f"{self.family_name} {self.given_name}"
