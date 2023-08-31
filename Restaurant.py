d1b1a00b3caea2b175a8e48da34e7797478476f0
Ian Raburu <williamraburu@gmail.com>
2023-08-31 12:01:27 +0300

    Create empty list and add restaurant instances

--- Restaurant.py
+++ Restaurant.py
@@ -1,8 +1,11 @@
 
 class Restaurant:
+    instances = []
     def __init__(self, name):
         self.name = name
+        Restaurant.instances.append(self)
+        self.reviews = []
 
     def name(self):
         return self.name
-
+    
