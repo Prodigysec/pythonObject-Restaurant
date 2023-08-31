ce8c2fbd0463969f041ca81a90f72d19e6d36dc5
Ian Raburu <williamraburu@gmail.com>
2023-08-31 11:24:12 +0300

    Return all Customer instances

--- Customer.py
+++ Customer.py
@@ -1,8 +1,12 @@
 
 class  Customer:
+    instances = []
+
     def __init__(self,given_name, family_name):
         self.given_name = given_name
         self.family_name = family_name
+
+        Customer.instances.append(self)
         
     def given_name(self):
         return self.given_name
@@ -12,3 +16,6 @@ class  Customer:
     
     def full_name(self):
         return f"{self.family_name} {self.given_name}"
+    
+    def all(self):
+        return Customer.instances
